669572040ac0fd62d03ed56cd41ed9888319027b
Anmol Gupta <111723339+anmolgupta313@users.noreply.github.com>
2025-06-09 14:28:16 -0400

    I have added function for sorting already used url and setting and getting articles from the local storage

--- src/app/Redux/createApi.js
+++ src/app/Redux/createApi.js
@@ -4,16 +4,20 @@ export const summaryApi = createApi({
   reducerPath: "summaryApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
-  }),
-  prepareHeaders: (headers) => {
-    headers.set("X-RapidAPI-Key", process.env.RAPID_API_KEY);
-    headers.set(
-      "X-RapidAPI-Host",
-      "article-extractor-and-summarizer.p.rapidapi.com"
-    );
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "66214e58cfmsh2a6c7e304eda1fap1a6507jsn69bb7c954a11"
+      );
+      headers.set(
+        "X-RapidAPI-Host",
+        "article-extractor-and-summarizer.p.rapidapi.com"
+      );
 
-    return headers;
-  },
+      return headers;
+    },
+  }),
+
   endpoints: (build) => ({
     getSummary: build.query({
       query: (query) =>
@@ -24,4 +28,4 @@ export const summaryApi = createApi({
   }),
 });
 
-export const { useGetSummary } = summaryApi;
+export const { useLazyGetSummaryQuery } = summaryApi;
